cdb11c8c5a69e50868fae2d13fc4d7ac61d05306
migmanu <jmanuelmigoya@gmail.com>
2022-01-12 21:04:49 -0300

    set secTrhesh to args[1]

--- pf_breacher.js
+++ pf_breacher.js
@@ -3,7 +3,7 @@
 // reduces sec level until given threshold or minimum level
 export async function main(ns) {
 	var target = ns.args[0];
-	var secThresh = ns.getServerMinSecurityLevel(target) + 5;
+	var secThresh = ns.args[1]
 	while (true) {
 		if (ns.getServerSecurityLevel(target) > secThresh) {
 			await ns.weaken(target);
